c54b64bd89e18beaddb4fd4267551779f9f945bd
masaki <maa22sa@gmail.com>
2020-07-04 22:16:11 +0900

    試しの初期データを入れてみる。

--- batch/insert_initial_data_to_db.py
+++ batch/insert_initial_data_to_db.py
@@ -13,9 +13,15 @@ TEST_USER_ID2 = "sample2"
 TEST_USER_ID3 = "sample3"
 TEST_USER_ID4 = "sample4"
 TEST_USER_ID5 = "sample5"
+TEST_USER_ID_LIST = [TEST_USER_ID1, TEST_USER_ID2, TEST_USER_ID3, TEST_USER_ID4, TEST_USER_ID5]
 
 conn = psycopg2.connect("postgresql://{}:{}@{}:{}/{}".format(user, password, host, port_db, database))
 cur = conn.cursor()
+
+
+def insert_votes(num, restaurant_name):
+    for i in range(max(num, 5)):
+        cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '{}')".format(TEST_USER_ID_LIST[i], restaurant_name))
 
 cur.execute("DROP TABLE IF EXISTS favorite_restaurants")
 cur.execute('''CREATE TABLE favorite_restaurants
@@ -25,42 +31,56 @@ cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '鼎泰豐')".format
 cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '華菜樓 ルミネ新宿店')".format(TEST_USER_ID_AOS))
 cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '石の家')".format(TEST_USER_ID_AOS))
 
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '鼎泰豐')".format(TEST_USER_ID1))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '鼎泰豐')".format(TEST_USER_ID2))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '鼎泰豐')".format(TEST_USER_ID3))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '華菜樓 ルミネ新宿店')".format(TEST_USER_ID1))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '華菜樓 ルミネ新宿店')".format(TEST_USER_ID2))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '石の家')".format(TEST_USER_ID1))
+print("中華")
+insert_votes(5, '鼎泰豐')
+insert_votes(4, '華菜樓 ルミネ新宿店')
+insert_votes(2, '隨園別館 新宿タカシマヤ タイムズスクエア店')
 
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '韓国料理スランジェ 新宿')".format(TEST_USER_ID1))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '韓国料理スランジェ 新宿')".format(TEST_USER_ID2))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '韓花')".format(TEST_USER_ID1))
+print("韓国")
+insert_votes(2, '韓国料理スランジェ 新宿')
+insert_votes(1, '韓花')
+insert_votes(1, '焼肉・韓国料理 KollaBo 新宿南口店')
 
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'PIZZERIA CAPOLI')".format(TEST_USER_ID1))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'PIZZERIA CAPOLI')".format(TEST_USER_ID2))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'PIZZERIA CAPOLI')".format(TEST_USER_ID3))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'ペッシェドーロ 新宿')".format(TEST_USER_ID1))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'ペッシェドーロ 新宿')".format(TEST_USER_ID2))
+print("イタリアン")
+insert_votes(4, 'PIZZERIA CAPOLI')
+insert_votes(2, 'ビラビアンキ 新宿店')
+insert_votes(2, '太陽のトマト麺NEXT 新宿ミロード店')
+insert_votes(2, 'Pizzeria＆Bar イタリアン チェルト！ 新宿南口店')
 
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'KICHIRI 新宿')".format(TEST_USER_ID1))
-cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'KICHIRI 新宿')".format(TEST_USER_ID2))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID3))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID1))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID2))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID1))
+print("和食")
+insert_votes(3, '隠れ房 御庭')
+insert_votes(2, 'KICHIRI 新宿')
+insert_votes(2, '黒毛和牛 腰塚')
+insert_votes(2, '大かまど飯　寅福 ルミネ新宿店')
+insert_votes(2, '個室風流 七色てまりうた 新宿')
 
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID1))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID2))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID3))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID1))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID2))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', '')".format(TEST_USER_ID1))
+print("カフェ")
+insert_votes(2, 'マンゴツリーカフェ ルミネ新宿')
+insert_votes(2, 'アマティ ルミネ１ルミネ新宿店')
+insert_votes(2, 'サラベス ルミネ新宿店')
+insert_votes(2, 'ラ・メゾン アンソレイユターブル ルミネ新宿店')
+insert_votes(2, 'HAND BAKES ルミネ新宿店')
+insert_votes(2, '京はやしや')
+insert_votes(2, 'アフタヌーンティー・ラブアンドテーブル ルミネ新宿店')
+insert_votes(2, 'モアナキッチンカフェ 新宿タカシマヤタイムズスクエア店')
 
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'サンマルクカフェ 新宿新南口店')".format(TEST_USER_ID1))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'サンマルクカフェ 新宿新南口店')".format(TEST_USER_ID2))
+print("居酒屋")
+insert_votes(2, '地酒と創作和食 九平次 新宿店')
+insert_votes(2, '居酒屋かあさん 新宿南口店')
+insert_votes(2, 'KICHIRI 新宿')
+insert_votes(2, '地産地鶏専門店 茂松 新宿南口店')
+insert_votes(2, '三代目 鳥メロ 新宿南口店')
+
+print("その他")
+insert_votes(2, 'J．S． BURGERS CAFE／ J．S． BEER GARDEN')
+insert_votes(2, '九州料理と完全個室 美味か 新宿店')
+insert_votes(2, 'とんかつ とん匠')
+insert_votes(2, '名代とんかつ かつくら 新宿高島屋店')
+insert_votes(2, '築地玉寿司 新宿高島屋店')
+
+
 # cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'ROSEMARY’S TOKYO')".format(TEST_USER_ID1))
 # cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'COBI COFFEE box')".format(TEST_USER_ID1))
-# cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'ブルーボトルコーヒー 新宿')".format(TEST_USER_ID1))
 # cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'ＯＴＴＩＭＯ Ｓｅａｆｏｏｄ ｇａｒｄｅｎ')".format(TEST_USER_ID1))
 # cur.execute("INSERT INTO favorite_restaurants VALUES ('{}', 'GARDEN HOUSE SHINJUKU')".format(TEST_USER_ID1))
 conn.commit() # Save (commit) the changes
